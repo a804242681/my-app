576ca033ebdd3b7618a24787bd910baaa5be8bc6
unknown <youare08@qq.com>
2018-06-18 10:58:16 -0400

    Add existing file

--- hunter/index.js
+++ hunter/index.js
@@ -1,6 +1,9 @@
+
 module.exports = robot => {
   robot.on('push', async context => {
     // Code was pushed to the repo, what should we do with it?
-  app.log('hunter,hunter')
+    robot.log(context)
+          robot.log("hunter")
+    
   })
 }
