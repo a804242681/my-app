7d23262b6535e05d3f0c59ab00f5912c302e4e3a
unknown <youare08@qq.com>
2018-06-19 09:58:01 -0400

    Add existing file

--- hunter/index.js
+++ hunter/index.js
@@ -1,8 +1,29 @@
-var i=1;
+const commands = require('probot-commands')
+
 module.exports = robot => {
-  robot.on(`*`, async context => {
+    var save =null
+
+    var i=1;
+    
+    robot.on(`*`, async context => {
+          
     context.log({event: context.event, action: context.payload.action})
       robot.log(i)
       i++
+            const { issue, comment, repository } = context.payload
+            const issueCopy = {
+      'owner': repository.owner.login,
+      'repo': repository.name,
+      'title': issue.title,
+      'body': `${issue.body}\n\nCopy of [#${issue.number}](${issue.html_url})`,
+      'milestone': issue.milestone ? issue.milestone.number : null,
+      'labels': issue.labels
+    }
+            
+    var dt = new Date();
+    save=repository.owner.login+" "+repository.name+" "+issue.title+" "+issue.labels+" " + (dt.getMonth() + 1) + "/" + dt.getDate()+"/"+dt.getFullYear()+" "+dt.getHours()+" hour "+dt.getMinutes()+"min "+ 
+       dt.getSeconds()+" second "
+          robot.log(save)
   })
+ 
 }
